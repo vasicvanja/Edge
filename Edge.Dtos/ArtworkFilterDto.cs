6747489e29451a12a4498cce32a4295e1e60e3e6
Vanja Vasic <vanja.vasic@yahoo.com>
2025-04-23 12:26:02 +0200

    allow null values for MinPrice and MaxPrice

--- Edge.Dtos/ArtworkFilterDto.cs
+++ Edge.Dtos/ArtworkFilterDto.cs
@@ -7,8 +7,8 @@ namespace Edge.Dtos
         public string Technique { get; set; }
         public EArtworkType? Type { get; set; }
         public int CycleId { get; set; }
-        public decimal MinPrice { get; set; }
-        public decimal MaxPrice { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
         public string SortBy { get; set; } // e.g. "price", "name"
         public string SortDirection { get; set; } = "asc"; // or "desc"
     }
