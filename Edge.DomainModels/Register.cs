804be34a34defbb1090ceb9d41e0603fae3a92af
Vanja Vasic <vanja.vasic@yahoo.com>
2025-06-18 01:17:13 +0200

    add first and last name to user

--- Edge.DomainModels/Register.cs
+++ Edge.DomainModels/Register.cs
@@ -4,13 +4,22 @@ namespace Edge.DomainModels
 {
     public class Register
     {
+        [Required(ErrorMessage = "First Name is required!")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required!")]
+        public string LastName { get; set; }
+
         [Required(ErrorMessage = "Username is required!")]
         public string Username { get; set; }
+
         [Required(ErrorMessage = "Email is required!")]
         public string Email { get; set; }
+
         [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$^+=!*()@%&]).{8,}$")]
         [Required(ErrorMessage = "Password is required!")]
         public string Password { get; set; }
+
         [Required(ErrorMessage = "Phone number is required!")]
         public string PhoneNumber { get; set; }
     }
